f25722b1788fdece6156c2fbb2e6f4965547463f
Abdullah Selek <abdullahselek@gmail.com>
2018-07-14 20:43:47 +0100

    Function that find images from given path.

---
+++ plant_disease_detection/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+'''Disease detection on different plants with Neural Networks.'''
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1'
+__url__          = 'https://github.com/abdullahselek/plant-disease-detection'
+__download_url__ = 'https://github.com/abdullahselek/plant-disease-detection'
+__description__  = 'Disease detection on different plants with Neural Networks.'
+
+from plant_disease_detection import (
+    datagenerator
+)
---
+++ plant_disease_detection/datagenerator.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import subprocess
+
+def find_images(path):
+    """
+    Returns an array with all image paths found dir.
+    Following extensions are used to filter images: 'jpg', 'png', 'JPG', 'PNG', 'JPEG', 'ppm', 'PPM', 'bmp', 'BMP'
+    Args:
+      dir (str):
+        Directory that contains images.
+    Returns:
+      image_paths (array).
+    """
+
+    directory = os.path.join('plant_disease_detection', path)
+    # Options for the GNU and BSD find command
+    extension_list = ['jpg', 'png','JPG','PNG','JPEG', 'ppm', 'PPM', 'bmp', 'BMP']
+    find_options = str.format('-iname "*.{0}"', extension_list[0])
+    for i in range(1, len(extension_list)):
+        find_options = str.format('{0} -o -iname "*.{1}"', find_options, extension_list[i])
+
+    # Find all the images using find command
+    print(str.format('find -L {0} {1}', directory, find_options))
+    process = subprocess.Popen([str.format('find -L {0} {1}', directory, find_options),],
+                                stdout=subprocess.PIPE,
+                                shell=True)
+    image_paths = []
+    while True:
+        line = process.stdout.readline()
+        if line != '':
+            filename = os.path.basename(line)
+            path = os.path.join(os.path.dirname(line), filename)
+            image_paths.append(path)
+        else:
+            break
+    return image_paths
