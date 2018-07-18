721277f30ce7b3ed497cef3da4bd19b2f9746c82
Abdullah Selek <abdullahselek@gmail.com>
2018-07-18 23:34:25 +0100

    Function to load training data.

--- plant_disease_detection/datagenerator.py
+++ plant_disease_detection/datagenerator.py
@@ -4,6 +4,8 @@
 import os
 import sys
 import subprocess
+import cv2
+import numpy as np
 
 def find_images(path):
     """
@@ -37,3 +39,31 @@ def find_images(path):
         else:
             break
     return image_paths
+
+def load_train_data(train_path, image_size, classes):
+    images = []
+    labels = []
+    img_names = []
+    class_array = []
+
+    image_paths = find_images('datasets/train')
+    print('Going to read training images')
+    for path in image_paths:   
+        image = cv2.imread(path)
+        image = cv2.resize(image,
+                        (image_size, image_size), 0,
+                        0, cv2.INTER_LINEAR)
+        image = image.astype(np.float32)
+        image = np.multiply(image, 1.0 / 255.0)
+        images.append(image)
+        label = np.zeros(len(classes))
+        label[0] = 1.0
+        labels.append('')
+        file_base = os.path.basename(file)
+        img_names.append(file_base)
+        class_array.append('')
+    images = np.array(images)
+    labels = np.array(labels)
+    img_names = np.array(img_names)
+    class_array = np.array(class_array)
+    return images, labels, img_names, class_array
