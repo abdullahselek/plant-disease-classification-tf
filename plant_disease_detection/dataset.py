036c508d26d27370c3b586fe727cd917bc315f5a
Abdullah Selek <abdullahselek@gmail.com>
2018-07-19 21:51:00 +0100

    Create dataset class with just init for now.

---
+++ plant_disease_detection/dataset.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+class DataSet(object):
+
+    def __init__(self, images, labels, img_names, cls):
+        self._num_examples = images.shape[0]
+        self._images = images
+        self._labels = labels
+        self._img_names = img_names
+        self._cls = cls
+        self._epochs_done = 0
+        self._index_in_epoch = 0
