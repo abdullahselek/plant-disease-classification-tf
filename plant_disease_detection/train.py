ecee279f67a3e4cf27ca7aac8c2e8ad934147216
Abdullah Selek <abdullahselek@gmail.com>
2018-07-26 20:37:36 +0100

    Create biases as a tf variable.

--- plant_disease_detection/train.py
+++ plant_disease_detection/train.py
@@ -34,3 +34,6 @@ def get_data():
 
 def create_weights(shape):
     return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+
+def create_biases(size):
+    return tf.Variable(tf.constant(0.05, shape=[size]))
