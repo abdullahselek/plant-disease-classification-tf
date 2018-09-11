33bd098a1e02c8b3e592584540bd40e73617fe16
Abdullah Selek <abdullahselek@gmail.com>
2018-09-11 21:39:47 +0100

    Restore latest checkpoint from session.

--- plant_disease_classification/predict.py
+++ plant_disease_classification/predict.py
@@ -6,6 +6,10 @@ import tensorflow as tf
 
 image_size = 128
 
+session = tf.Session()
+saver = tf.train.import_meta_graph('plants-disease-model.meta')
+saver.restore(session, tf.train.latest_checkpoint('./'))
+
 def predict(filename):
     image = cv2.imread(filename)
     # Resizing the image to our desired size and
