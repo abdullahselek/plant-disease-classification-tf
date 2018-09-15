13f9b02d0c044977ea51219d71c190c58f799409
Abdullah Selek <abdullahselek@gmail.com>
2018-09-15 14:16:07 +0100

    Create a help message for CLI menu.

--- plant_disease_classification/__main__.py
+++ plant_disease_classification/__main__.py
@@ -8,3 +8,17 @@ from plant_disease_classification import (
     datagenerator,
     predict
 )
+
+help_message = '''
+  Disease classification on different plants with using Machine Learning and Convolutional Neural Networks.
+  Usage
+    $ python plant_disease_classification [<options>]
+  Options
+    --help, -h              Display help message
+    --predict, -p           Predict image from given file path
+    --version, -v           Display installed version
+  Examples
+    $ python plant_disease_classification --help
+    $ python plant_disease_classification --predict
+    $ python plant_disease_classification --version
+'''
